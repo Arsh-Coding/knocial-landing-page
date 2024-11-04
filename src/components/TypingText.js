2acad3d91943207cdc5fb99d92295dae58aea3ac
Arshpreet Singh <karanarshpreet123@gmail.com>
2024-11-04 12:07:19 +0530

    react-scroll issue addressed

--- src/components/TypingText.js
+++ src/components/TypingText.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import styles from "../Styles/Typing.module.css";
 const TypingText = ({ texts, speed, delayBetweenTexts }) => {
   const [displayedText, setDisplayedText] = useState("");
   const [index, setIndex] = useState(0);
@@ -29,7 +29,7 @@ const TypingText = ({ texts, speed, delayBetweenTexts }) => {
     }
   }, [charIndex, isDeleting, index, speed, texts, delayBetweenTexts]);
 
-  return <p style={{ fontSize: "1.5rem", color: "white"}}>{displayedText}</p>;
+  return <p className={styles.typingtext}>{displayedText}</p>;
 };
 
 export default TypingText;
